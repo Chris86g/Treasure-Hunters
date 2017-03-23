3547da6b47e630fad0597de3c09d2deae0866a0b
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-03-23 20:28:39 +0100

    audio: intro, wave

--- js/app.js
+++ js/app.js
@@ -6,9 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var button = document.querySelector('button');
 
+  var introMP3 = document.querySelector('.intro audio');
+  var waveWAV = document.querySelector('.wave');
+  var eFX = document.querySelector('.eFX');
+
+  introMP3.play();
+
   button.addEventListener('click', function() {
     secIntro.style.display = "none";
     secBackground.style.display = "block";
+    introMP3.pause();
+    waveWAV.loop = true;
+    waveWAV.play();
+    eFX.play();
 
   })
 })
